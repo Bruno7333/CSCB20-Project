9f89234d21b094b2a06c23e1b592e1c66c032d5a
Bruno7333 <bruno.radu.home@gmail.com>
2026-03-25 20:05:38 -0400

    made progress :)

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = 'lebron-james-king-of-basketball'
 DB_FILE = "./SQLite/nba.db"
+leagues = ()
 
 def init_db():
     conn = sqlite3.connect(DB_FILE)
@@ -26,6 +28,28 @@ def login_page():
 def register_page():
     return render_template('register.html')
 
+@app.route("/league/<int:league_id>")
+def league(league_id):
+    conn = sqlite3.connect(DB_FILE)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT LID as id, leagueName FROM PlayerLeague WHERE LID = ?", 
+        (league_id,)
+        )
+    league = cursor.fetchone()
+    cursor.execute(
+        "SELECT pt.teamName as team_name, pa.username as name FROM PlayerTeam pt JOIN PlayerAccount pa on pt.accountID = pa.accountID WHERE pt.LID = ?",
+        (league_id,)
+        )
+    members = cursor.fetchall()
+    conn.close()
+
+    if league is None:
+        return redirect(url_for('dashboard'))
+
+    return render_template("league.html", league=league, members=members)
+
 @app.route('/dashboard', methods=['GET'])
 def dashboard():
     conn = sqlite3.connect(DB_FILE)
@@ -33,7 +57,10 @@ def dashboard():
     cursor = conn.cursor()
 
     # select leagues
-    cursor.execute("SELECT LID as id, leagueName as name FROM PlayerLeague")
+    cursor.execute(
+        "SELECT pl.LID as id, pl.leagueName as name FROM PlayerLeague pL JOIN PlayerTeam pt ON pl.LID = pt.LID WHERE pt.accountId = ?",
+        (session.get('user_id'),)       
+        )
     leagues = cursor.fetchall()
     conn.close()
 
@@ -43,13 +70,18 @@ def dashboard():
 def create_league():
     league_name = request.form['leagueName']
     draft_type = request.form['draftType']
-    owner_id = 1
+    owner_id = session.get('user_id')
 
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
     cursor.execute(
         "INSERT INTO PlayerLeague (leagueName, draftType, ownerAccount, status) VALUES (?, ?, ?, 'initial')",
         (league_name, draft_type, owner_id)
+    )
+    new_league_id = cursor.lastrowid
+    cursor.execute(
+        "INSERT INTO PlayerTeam (LID, accountID, teamName) VALUES (?, ?, ?)",
+        (new_league_id, owner_id, "My Team")
     )
     conn.commit()
     conn.close()
@@ -78,6 +110,7 @@ def login():
     password = request.form['password']
 
     conn = sqlite3.connect(DB_FILE)
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
 
     # Use PlayerAccount table
@@ -87,6 +120,7 @@ def login():
     conn.close()
 
     if user:
+        session['user_id'] = user['accountID']
         return redirect(url_for('dashboard'))
     else:
         return "Login failed. Check your credentials."
